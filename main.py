678704229b892a0677e3522b2f9107b58741be96
dev2l0per <z-_-z1-@naver.com>
2022-03-10 16:18:16 +0900

    Remove debug mode

--- main.py
+++ main.py
@@ -292,4 +292,4 @@ def healthCheck():
 threading.Thread(target=handle_requests_by_batch).start()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
